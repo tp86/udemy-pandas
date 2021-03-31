0a6d90453d4c3bf141dd019637fabf8b44e160e1
tp86 <tom0palka+tp86@gmail.com>
2021-03-31 14:54:20 +0000

    Configure container, add first scripts

---
+++ src/01_series_creation.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+# Example 1
+s = pd.Series(4)
+
+# Example 2
+s_def = pd.Series(data=[21, 34, 54], index=[
+                  'adam', 'tomek', 'paweł'], name='age')
