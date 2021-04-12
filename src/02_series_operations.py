e72d1bf587c1b4e88f9e9aaa687d6eb67cb9d316
tp86 <tom0palka+tp86@gmail.com>
2021-04-12 14:18:47 +0000

    More operations on Series

--- src/02_series_operations.py
+++ src/02_series_operations.py
@@ -6,7 +6,7 @@ import numpy as np
 np.random.seed(0)
 
 A = np.random.randint(0, 10, 10)
-series = pd.Series(A, name='los')
+series = pd.Series(A, name="los")
 
 # %% attributes
 series.dtype
@@ -33,3 +33,41 @@ series_N.divide(series_M)
 series.drop_duplicates()
 series[4] = np.nan
 series.dropna()
+
+series.idxmax()
+series.idxmin()
+
+series.count()
+
+series_N.cumsum()
+series_N.cummin()
+series_N.cummax()
+
+series.value_counts()
+series.sum()
+series.std()
+series.describe()
+
+# %% histogram
+hist_data = pd.Series(np.random.randn(10000))
+hist_data.hist(bins=80)
+
+# %% top n
+series_N.nlargest(3)
+
+# %% quantiles
+q_N = series_N.quantile(0.25)
+median = series_N.quantile(0.5)
+
+# %% rounding
+series_N.round(3)
+
+# %% shifting
+shifted_1 = series.shift(1)
+# will replace NA only for new values
+shifted_2_replace_0 = series.shift(2, fill_value=0)
+# will replace all NAs, even existing ones
+shifted_2_replace_0 = series.shift(2).fillna(0)
+
+# %% sorting
+series.sort_values()
